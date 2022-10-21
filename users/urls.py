bbb3747908d0aafe479ce9fdb31321eac58f96d5
20521143 <20521143@gm.uit.edu.vn>
2022-10-21 14:36:03 +0700

    add new app users

---
+++ users/urls.py
@@ -0,0 +1,11 @@
+from . import views
+from django.urls import path
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('login', views.login_view, name='login'),
+    path('logout', views.logout_view, name='logout'),
+    path("register", views.register, name="register"),
+    path("resetpwd", views.resetpwd, name="resetpwd"),
+    path('update', views.update, name='update')
+]
