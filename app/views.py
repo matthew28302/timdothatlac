d4f3f892c5a7aee8dd9a69af60a8084dc4b449d7
CongNguyen123 <nguyenthanhlong012002@gmail.com>
2022-10-22 12:09:25 +0700

    add new data

--- app/views.py
+++ app/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
+from .models import Item, menuItem
 
 # Create your views here.
 def index(request):
-    return render(request,'app/index.html')
+    
+    return render(request,'app/index.html',{
+        "items": Item.objects.all()
+    })
 
 def post(request):
     return render(request, 'app/postNews.html' )
+
+# detail  information of the item
+def item(request, item_id):
+    item = Item.objects.get(id=item_id)
+    return render(request, "app/item.html", {
+        "item": item
+    })
